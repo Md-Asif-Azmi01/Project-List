1aa63bb32f6223206aa8e24979040174feb2864c
Md Asif Azmi <mdasifazmi01@gmail.com>
2026-06-28 02:49:06 +0530

    changes: change for error

---
+++ pages/500.jsx
@@ -0,0 +1,9 @@
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>500 - Server Error</h1>
+      <p>Something went wrong on our end. Please try again later.</p>
+      <a href="/">Go back home</a>
+    </div>
+  );
+}
